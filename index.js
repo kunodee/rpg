2b9922baadb3099216b4482e65995e1c6cc943aa
fede <borronifederico6@gmail.com>
2025-02-06 14:56:11 +0100

    aggiunta dei mostri, cambio dei punteggi vita, attacco, difesa e possibilità di scappare

--- index.js
+++ index.js
@@ -7,46 +7,81 @@ function random_number(min, max) {
 
 let user = {
     name: prompt("Inserisci il tuo nome da combattente: "),
-    health: random_number(10000, 15000),
-    attack: random_number(9000, 12000),
+    health: 100,
+    attack: random_number(15, 22),
+    defense: random_number(6, 10),
     potions: 3
 }
 
-monsters = ["cremisi", "oblio"]
-
-const cremisi = {
-    name: "Drago dell'Abisso Cremisi",
-    health: random_number(23000, 25000),
-    attack: random_number(800, 1500),
-    defense: random_number(300, 700),
-    runaway: 30
-}
+monsters = ["oblio", "cremisi", "guardiano", "mietitore", "oracolo", "titano", "cavaliereNero"]
 
 const oblio = {
-    name: "Reventant dell'Oblio",
-    health: random_number(9000, 10500),
-    attack: random_number(600, 1100),
-    defense: random_number(200, 500),
-    runaway: 80
-}
+    name: "Revenant dell’Oblio",
+    health: random_number(120, 180),
+    attack: random_number(40, 60),
+    defense: random_number(8, 18),
+    runaway: 40
+};
+
+const guardiano = {
+    name: "Guardiano dell’Ombra Infranta",
+    health: random_number(180, 250),
+    attack: random_number(55, 80),
+    defense: random_number(12, 26),
+    runaway: 25
+};
+
+const mietitore = {
+    name: "Bestia della Mietitura Eterna",
+    health: random_number(140, 200),
+    attack: random_number(45, 70),
+    defense: random_number(9, 22),
+    runaway: 35
+};
+
+const oracolo = {
+    name: "Oracolo della Luna Insanguinata",
+    health: random_number(130, 190),
+    attack: random_number(42, 65),
+    defense: random_number(7, 20),
+    runaway: 38
+};
+
+const titano = {
+    name: "Titano dell’Ultimo Sigillo",
+    health: random_number(200, 280),
+    attack: random_number(60, 90),
+    defense: random_number(14, 30),
+    runaway: 20
+};
+
+const cavaliereNero = {
+    name: "Cavaliere del Sole Nero",
+    health: random_number(170, 240),
+    attack: random_number(55, 85),
+    defense: random_number(12, 28),
+    runaway: 25
+};
 
 let monster = monsters[random_number(0, monsters.length - 1)]
 
 switch (monster) {
-    case "cremisi":
-        monster = cremisi
-        break
     case "oblio":
         monster = oblio
         break
+    case "guardiano":
+        monster = guardiano
+        break
+    case "mietitore":
+        monster = mietitore
+        break
+    case "oracolo":
+        monster = oracolo
+        break
+    case "titano":
+        monster = titano
+        break
+    case "cavaliereNero":
+        monster = cavaliereNero
+        break
 }
-
-function statistics(){
-    console.log(`STATS | ${monster.name}: ${monster.health} HP, ${monster.attack} ATK, ${monster.defense} DEF`)
-    console.log(`STATS | ${user.name}: ${user.health} HP, ${user.attack} ATK, ${user.potions} PZ`)
-}
-
-console.log("")
-console.log(`FIGHT | ${monster.name} ti ha attaccato!`)
-statistics()
-console.log("")
