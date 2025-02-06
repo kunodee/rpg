701d909ae0cfb0c22f5016fec2ebf8118d9ac454
fede <borronifederico6@gmail.com>
2025-02-06 14:12:12 +0100

    user definition with random numbers

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+function random_number(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  }
+
+let user = {
+    name: prompt("Inserisci il tuo nome da combattente: "),
+    health: 100,
+    attack: random_number(8, 15),
+    potions: 3
+}
